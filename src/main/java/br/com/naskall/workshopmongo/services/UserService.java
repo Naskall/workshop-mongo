006733d76dee2024cc40bfec3ded4a32fcf596b1
Kauê Kuhn <ksn7@ymail.com>
2019-10-18 11:24:41 -0300

    Inserção de usuarios com POST

--- src/main/java/br/com/naskall/workshopmongo/services/UserService.java
+++ src/main/java/br/com/naskall/workshopmongo/services/UserService.java
@@ -1,11 +1,14 @@
 package br.com.naskall.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.naskall.workshopmongo.domain.User;
+import br.com.naskall.workshopmongo.dto.UserDTO;
 import br.com.naskall.workshopmongo.repository.UserRepository;
+import br.com.naskall.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,6 +17,21 @@ public class UserService {
 
   public List<User> findAll() {
     return repo.findAll();
+  }
+
+  public User findById(String id) {
+    Optional<User> obj = repo.findById(id);
+
+    return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+  }
+
+  public User insert(User obj) {
+    return repo.insert(obj);
+
+  }
+  
+  public User fromDTO(UserDTO objDto){
+    return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
   }
 
 }
