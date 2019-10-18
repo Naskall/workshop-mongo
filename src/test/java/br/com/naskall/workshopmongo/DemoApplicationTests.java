40e565d2582aeac367cf58b2a30655609de31836
Kauê Kuhn <ksn7@ymail.com>
2019-10-17 21:27:23 -0300

    first commit

---
+++ src/test/java/br/com/naskall/workshopmongo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.naskall.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
